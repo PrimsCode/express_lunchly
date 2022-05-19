a0129a24b2523eb9a0670011fd405463eed19149
Prima <primabreuerjenkins@gmail.com>
2022-05-19 15:31:23 -0500

    complete assignment

---
+++ db.js
@@ -0,0 +1,19 @@
+/** Database for lunchly */
+
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql://postgres:admin@localhost:5432/lunchly";
+} else {
+    DB_URI = "postgresql://postgres:admin@localhost:5432/lunchly";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
---
+++ routes.js
@@ -0,0 +1,152 @@
+/** Routes for Lunchly */
+
+const express = require("express");
+
+const Customer = require("./models/customer");
+const Reservation = require("./models/reservation");
+
+const router = new express.Router();
+
+/** Homepage: show list of customers. */
+
+router.get("/", async function(req, res, next) {
+  try {
+    const customers = await Customer.all();
+    return res.render("customer_list.html", { customers });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Form to add a new customer. */
+
+router.get("/add/", async function(req, res, next) {
+  try {
+    return res.render("customer_new_form.html");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Handle adding a new customer. */
+
+router.post("/add/", async function(req, res, next) {
+  try {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phone = req.body.phone;
+    const notes = req.body.notes;
+
+    const customer = new Customer({ firstName, lastName, phone, notes });
+    await customer.save();
+
+    return res.redirect(`/${customer.id}/`);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Show a customer, given their ID. */
+
+router.get("/:id/", async function(req, res, next) {
+  try {
+    const customer = await Customer.get(req.params.id);
+    const reservations = await customer.getReservations();
+
+    return res.render("customer_detail.html", { customer, reservations });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/**Working on search bar */
+
+// router.get("/getCustomer", async function (req, res, next) {
+//   try {
+//     let payload = req.body.payload.trim();
+//     let search = await Customer.find({firstName: new RegExp('^'+payload+'.*', 'i')}).exec();
+//     search = search.slice(0, 10);
+//     return res.send({payload: search});
+
+//   } catch(err) {
+//     return next(err);
+//   }
+
+// })
+
+
+// router.get("/search/:title", async function (req, res, next) {
+//   try {
+//     let title = req.params.title;
+//     let search = await Customer.find({firstName: new RegExp('^'+title+'.*', 'i')}).exec();
+//     searchName = search.slice(0, 10);
+
+//     const foundCustomer = await Customer.find(searchName);
+//     const customer = await Customer.get(foundCustomer.id);
+//     const reservations = await customer.getReservations();
+
+//     return res.render("customer_detail.html", { customer, reservations });
+
+//   } catch(err) {
+//     return next(err);
+//   }
+
+// })
+
+/** Show form to edit a customer. */
+
+router.get("/:id/edit/", async function(req, res, next) {
+  try {
+    const customer = await Customer.get(req.params.id);
+
+    res.render("customer_edit_form.html", { customer });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Handle editing a customer. */
+
+router.post("/:id/edit/", async function(req, res, next) {
+  try {
+    const customer = await Customer.get(req.params.id);
+    customer.firstName = req.body.firstName;
+    customer.lastName = req.body.lastName;
+    customer.phone = req.body.phone;
+    customer.notes = req.body.notes;
+    await customer.save();
+
+    return res.redirect(`/${customer.id}/`);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Handle adding a new reservation. */
+
+router.post("/:id/add-reservation/", async function(req, res, next) {
+  try {
+    const customerId = req.params.id;
+    const startAt = new Date(req.body.startAt);
+    const numGuests = req.body.numGuests;
+    const notes = req.body.notes;
+
+    const reservation = new Reservation({
+      customerId,
+      startAt,
+      numGuests,
+      notes
+    });
+    await reservation.save();
+
+    return res.redirect(`/${customerId}/`);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+
+
+
+module.exports = router;
